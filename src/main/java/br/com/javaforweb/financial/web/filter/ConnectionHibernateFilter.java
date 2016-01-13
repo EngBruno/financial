4ff9f299b23673bbf8b195de3b02b0c6f5fea445
Bruno Roberto <roberto.bruno007@gmail.com>
2016-01-13 17:10:25 -0300

    Adicionando Arquivos de configuracao do hibernate, Criando classe do Padrão DAO, implmentando UserDAO.

--- src/main/java/br/com/javaforweb/financial/web/filter/ConnectionHibernateFilter.java
+++ src/main/java/br/com/javaforweb/financial/web/filter/ConnectionHibernateFilter.java
@@ -1,5 +1,52 @@
 package br.com.javaforweb.financial.web.filter;
 
-public class ConnectionHibernateFilter {
+import java.io.IOException;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import br.com.javaforweb.financial.util.HibernateUtil;
+
+public class ConnectionHibernateFilter implements Filter {
+
+	private SessionFactory sessionFactory;
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		this.sessionFactory = HibernateUtil.getSessionFactory();
+	}
+	
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+			throws IOException, ServletException {
+		Session currentSession = this.sessionFactory.getCurrentSession();
+		
+		Transaction transaction = null;
+		try{
+			transaction = currentSession.beginTransaction();
+			chain.doFilter(servletRequest, servletResponse);
+			transaction.commit();
+			if(currentSession.isOpen()){
+				currentSession.close();
+			}
+		}catch(Throwable ex){
+			try{
+				if(transaction.isActive()){
+					transaction.rollback();
+				}
+			}catch(Throwable t){
+				t.printStackTrace();
+			}
+			throw new ServletException(ex);
+		}
+	}
+	@Override
+	public void destroy() {}
 }
